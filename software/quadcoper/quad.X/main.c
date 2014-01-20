e67778c0c0969327c2dc3675590e7c64e04109f8
linuxesdios <linuxesdios@gmail.com>
2014-01-20 03:25:05 +0100

    filtro complemtario realizado (chapuza)
    realizado filtro de forma muy basica sin calcular nada mas que
    moviemiento sin estudiarlo

--- software/quadcoper/quad.X/main.c
+++ software/quadcoper/quad.X/main.c
@@ -24,17 +24,23 @@ _FWDT(WDT_OFF)
 _FBORPOR(PWRT_OFF & BORV27 & PBOR_OFF & MCLR_DIS)
 // FIN FUSES
 
+//**************************borrar*********************************
+    #define ACCELEROMETER_SENSITIVITY 892.0
+#define GYROSCOPE_SENSITIVITY 6.536
+
+#define M_PI 3.14159265359
+
+#define dt 1
+float pitch, roll;
+//*************************borrar*****************************
 
 
 int main(void)
 {
     //#define DATO_KALMAN
     //#define ARRANQUE
-volatile int chanel1 = 0;
-volatile int chanel2 = 0;
-volatile int chanel3 = 0;
-volatile int chanel4 = 0;
-volatile int chanel5 = 0;
+    
+
 
 #define CALIBRADO
     int calibra_ax, calibra_ay, calibra_az, calibra_gx, calibra_gy, calibra_gz, ax, ay, az, gx, gy, gz;
@@ -77,7 +83,10 @@ set_inicial();
         if (get_who_I_AM() == 104)
         {
         get_acelerometro(&ax, &ay, &az, &gx, &gy, &gz);
-        plot4(ax,ay,az,gx);
+        ComplementaryFilterax(ax,ay,az,gx,gy,gz);
+        plot4(ax,ay,pitch,roll);
+        enviar_valor("pitch", pitch);
+        enviar_valor("roll", roll);
         DelayXmsT1(10);
 
         }
@@ -96,4 +105,28 @@ set_inicial();
        
     }
     return 0;
+}
+
+
+void ComplementaryFilterax(ax,ay,az,gx,gy,gz)
+{
+    float pitchAcc, rollAcc;
+
+    // Integrate the gyroscope data -> int(angularSpeed) = angle
+    pitch += ((float)gx / GYROSCOPE_SENSITIVITY) * dt; // Angle around the X-axis
+    roll -= ((float)gy / GYROSCOPE_SENSITIVITY) * dt;    // Angle around the Y-axis
+
+    // Compensate for drift with accelerometer data if !bullshit
+    // Sensitivity = -2 to 2 G at 16Bit -> 2G = 32768 && 0.5G = 8192
+    int forceMagnitudeApprox = abs(ax) + abs(ay) + abs(az);
+    if (forceMagnitudeApprox > 8192 && forceMagnitudeApprox < 32768)
+    {
+	// Turning around the X axis results in a vector on the Y-axis
+        pitchAcc = atan2f((float)ay, (float)az) * 180 / M_PI;
+        pitch = pitch * 0.98 + pitchAcc * 0.02;
+
+	// Turning around the Y axis results in a vector on the X-axis
+        rollAcc = atan2f((float)ax, (float)az) * 180 / M_PI;
+        roll = roll * 0.98 + rollAcc * 0.02;
+    }
 }
